2643b2fe2385cbb779fa3c7a2d88e6de5bc83215
wendy <enyinnapc16127@gmail.com>
2022-02-06 07:58:45 -0800

    Render template using ejs

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ const debug = require('debug')('app');
 const morgan = require('morgan')
 const path = require('path')
 
+const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(morgan('tiny'))
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public/')))
 
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 app.get('/', (req, res)=> {
-    res.send('Back')
+    res.render('index.ejs')
 })
 
-app.listen(3000, ()=>{
-    debug(`App is listening on port ${chalk.green('3000')}`)
+app.listen(PORT, ()=>{
+    debug(`App is listening to port ${chalk.green(PORT)}`)
 })
